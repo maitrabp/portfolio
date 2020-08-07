7b0c79263c1eb6e28bf7ed77b1518504c47433b3
Maitra Patel <maitrapatel@yahoo.com>
2020-08-07 14:05:36 -0400

    Email Fix

--- index.js
+++ index.js
@@ -25,18 +25,28 @@ app.post('/api/form', (req, res) =>
                             <p>${req.body.message}</p>`
         let transporter = nodemailer.createTransport({
             pool: true,
-            service: 'gmail',
+            service: 'Gmail',
             auth: {
                 type: 'OAuth2',
                 user:  process.env.REACT_APP_USERNAME,
                 accessToken: process.env.REACT_APP_ACCESSTOKEN,
-                expires: 1593109815926 + 60000,
+                expires: 1596822687955 + 600000,
                 refreshToken: process.env.REACT_APP_REFRESHTOKEN,
                 clientId: process.env.REACT_APP_CLIENT_ID,
                 clientSecret: process.env.REACT_APP_CLIENT_SECRET,
                 accessUrl: process.env.REACT_APP_ACCESS_URL,
             
             },
+        })
+        transporter.verify((error, success) => {
+            if (error) return console.log(error)
+            console.log('Server is ready to take our messages: ', success)
+            transporter.on('token', token=> {
+                console.log('A new access token was generated')
+                console.log('User: %s', token.user)
+                console.log('Access Token: %s', token.accessToken)
+                console.log('Expires: %s', token.expires)
+            })
         })
         //fixed!
         let mailOptions = {
